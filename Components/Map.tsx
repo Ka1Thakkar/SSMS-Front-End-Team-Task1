6be7d404b668f358a65623ae2ba60c82b240110e
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2023-08-13 21:49:12 +0530

    Adjusted the zoom level of the Map component

--- Components/Map.tsx
+++ Components/Map.tsx
@@ -3,7 +3,7 @@ import { Map, Marker } from "pigeon-maps"
 
 export default function MyMap() {
   return (
-    <Map height={500} defaultCenter={[36.20740085393897, -115.26789593248834]} defaultZoom={12} metaWheelZoom={true}>
+    <Map height={500} defaultCenter={[36.20740085393897, -115.26789593248834]} defaultZoom={16} metaWheelZoom={true}>
       <Marker width={40} anchor={[36.20740085393897, -115.26789593248834]} />
     </Map>
   )
